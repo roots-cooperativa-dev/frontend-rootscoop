83ad3b2d82903e119e56e905393b4ffe20272513
kevin-castro-96 <street123castle@gmail.com>
2025-07-10 14:30:05 -0300

    cambie metadata

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Cooperativa Roots',
+  description: 'Sitio web de cooperativa de trabajo Roots',
   generator: 'v0.dev',
 }
 
