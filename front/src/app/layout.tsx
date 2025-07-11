cc5fe2902e98e05f44d6d2e7516bf12d846c89a5
kevin-castro-96 <street123castle@gmail.com>
2025-07-11 18:15:53 -0300

    elimino head

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Roots Cooperativa',
-  description: 'Roots Cooperativa de Trabajo - Construyendo Soberanía Alimentaria y Economía Social desde 2013',
-  generator: 'Next.js',
-}
+  title: "Roots Cooperativa",
+  description:
+    "Roots Cooperativa de Trabajo - Construyendo Soberanía Alimentaria y Economía Social desde 2013",
+  generator: "Next.js",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
