58d516caabee739a37be3da88ebfd77e4082e391
kevin-castro-96 <street123castle@gmail.com>
2025-07-21 01:08:04 -0300

    agrego actualizar datos de usuario

--- front/src/app/profile/page.tsx
+++ front/src/app/profile/page.tsx
@@ -1,37 +1,17 @@
 "use client";
-import Link from "next/link";
+
 import DataUser from "./component/dataUser";
-import Image from "next/image";
 import { Footer } from "@/src/components/landing/Footer";
-import ButtonLogout from "@/src/components/botones/cerrarSesion";
-import ButtonHome from "@/src/components/botones/verSitio";
 import Sidebar from "@/src/components/contenedores/sidebar";
+import HeaderProfile from "@/src/components/headers/EncabezadoPerfil";
 
 const Profile = () => {
 
   return (
     <>
-      <header className="bg-white border-b shadow-sm">
-        <div className="container mx-auto px-4 py-4">
-          <div className="flex items-center justify-between">
-            <Link href="/" className="flex">
-              <Image
-                src="/logos/roots.png"
-                alt="Rootscoop Logo"
-                width={60}
-                height={40}
-                className="rounded-full object-scale-down"
-                priority
-              />
-            </Link>
-            <div className="flex items-center space-x-4">
-              <ButtonHome/>
-              <ButtonLogout/>
-            </div>
-          </div>
-        </div>
-      </header>
+      <HeaderProfile/>
       <div className="flex">
+        <Sidebar/>
         <DataUser />
       </div>
       <Footer/>
