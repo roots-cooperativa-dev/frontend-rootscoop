58caf9dc1b8b8fcb6279d3e9fbf77e206b1b228b
kevin-castro-96 <street123castle@gmail.com>
2025-07-28 20:21:18 -0300

    corregi pagina de donaciones

--- front/src/services/cart.tsx
+++ front/src/services/cart.tsx
@@ -14,17 +14,20 @@ const axiosApiBack = axios.create({
   },
 });
 
-export const addProductToCart = async (data: Icart, token: string | null | undefined) => {
+export const addProductToCart = async (
+  data: Icart,
+  token: string | null | undefined
+) => {
   try {
-    console.log(data)
-    console.log(token)
+    console.log(data);
+    console.log(token);
     const response = await axiosApiBack.post("/orders/cart/add", data, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
 
-    if (!response.data) throw new Error("No se pudo añadir al carrito");
+    if (!response.data) throw new Error("Problema para añadir al carrito");
 
     return response.data;
   } catch (e: any) {
@@ -33,7 +36,7 @@ export const addProductToCart = async (data: Icart, token: string | null | undef
   }
 };
 
-export const getCart = async ( token: string | null | undefined) => {
+export const getCart = async (token: string | null | undefined) => {
   try {
     const response = await axiosApiBack.get("/orders/cart", {
       headers: {
@@ -43,7 +46,7 @@ export const getCart = async ( token: string | null | undefined) => {
 
     console.log("RESPONSE DATA:", response.data); // 👈 esto es clave
     if (!response.data) throw new Error("No hay productos en carrito");
-    console.log(response)
+    console.log(response);
     return response.data;
   } catch (e: any) {
     console.warn(e.message);
@@ -51,7 +54,10 @@ export const getCart = async ( token: string | null | undefined) => {
   }
 };
 
-export const deleteCartItem = async ( id: string | null | undefined, token: string) => {
+export const deleteCartItem = async (
+  id: string | null | undefined,
+  token: string | null | undefined
+) => {
   try {
     const response = await axiosApiBack.get(`/orders/cart/items/${id}`, {
       headers: {
