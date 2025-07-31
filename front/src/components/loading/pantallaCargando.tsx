d29c55b5dd8a4c6ece704a85b54e63aafc3f7ca7
kevin-castro-96 <street123castle@gmail.com>
2025-07-31 01:13:54 -0300

    arreglo callback google

--- front/src/components/loading/pantallaCargando.tsx
+++ front/src/components/loading/pantallaCargando.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <>
-      <div className="flex justify-center items-center py-20 w-screen">
+      <div className="flex justify-center items-center py-20 w-screen h-screen">
         <div className="text-center space-y-4">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-[#017d74] mx-auto"></div>
           <p className="text-gray-500">Cargando</p>
